6289eddf9f288699f17019b4147e08cd84fa4ac8
John <gijohny17@gmail.com>
2021-04-20 15:51:36 +0100

    fixed scripts in js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,8 @@
- // Data Table Filtering in Search and Numbers
- $(document).ready(function() {
-    var dataTable = $('#filtertable').DataTable({
-        responsive: true,
-        "pageLength":10,
-        columnDefs:[
-            {type:'date-dd-mm-yyyy',aTargets:[3]}
-        ],                
-        "bLengthChange":false,        
-    });
-    $("#filterbox").keyup(function(){
-        dataTable.search(this.value).draw();
-    });
-} );
+function myFunction() {
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+    x.type = "text";
+    } else {
+    x.type = "password";
+    }
+   }
